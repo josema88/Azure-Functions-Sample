c41955892077e0f8590ce6211e17c7cbaba82b46
Jose Ordoñez <josemaord@gmail.com>
2021-02-07 01:06:06 -0600

    Add Delete operation Function and implement Serializator as static object

--- DepartamentosFunctions.cs
+++ DepartamentosFunctions.cs
@@ -17,6 +17,8 @@ namespace Company.Function
 {
     public static class DepartamentosFunctions
     {
+        private static Serializator serializator = new Serializator();
+        
         [FunctionName("InsertDepartaments")]
         public static async Task<IActionResult> Insert(
             [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "departamentos")] HttpRequest req,
@@ -100,7 +102,7 @@ namespace Company.Function
                         cmd.Connection = connection;
                         connection.Open();
                         dataReader = cmd.ExecuteReader();
-                        data = new Serializator().Serialize(dataReader);
+                        data = serializator.Serialize(dataReader);
                         if(((IEnumerable<object>)data).Count()==1)
                             data=((IEnumerable<object>)data).First();
                     }	
@@ -165,6 +167,56 @@ namespace Company.Function
             return !successful
                     ? new BadRequestObjectResult("The request failed")
                     : (ActionResult)new OkObjectResult($"Data for id {id} update succesfully");
+        }
+        
+        [FunctionName("DeleteDepartaments")]
+        public static async Task<IActionResult> Delete(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "delete", Route = "departamentos/{id:int}")] HttpRequest req,
+            int id, ILogger log, ExecutionContext context)
+        {
+            log.LogInformation("Delete");
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            string name = data?.name;
+
+            var config = new ConfigurationBuilder()
+                            .SetBasePath(context.FunctionAppDirectory)
+                            .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
+                            .AddEnvironmentVariables()
+                            .Build();
+            string connectionString = config["ConnectionString"];
+
+            var successful = false;
+            log.LogInformation($"Parameter: {name}");
+            if(!String.IsNullOrEmpty(name))
+            {
+                try
+                {
+                    using (SqlConnection connection = new SqlConnection(connectionString))
+                    {
+                        // Delete
+                        string query = String.Format("DELETE FROM [dbo].[departamento] WHERE [id]={1}", id);
+                        using (SqlCommand cmd = new SqlCommand(query, connection))
+                        {
+                            connection.Open();
+                            cmd.ExecuteNonQuery();
+                            connection.Close();
+                        }	
+                    }
+                    successful = true;
+                }
+                catch (Exception x)
+                {
+                    log.LogInformation("exception: " + x.StackTrace.ToString());
+                    successful = false;
+                }	
+            }
+            else
+                successful = false;
+            return !successful
+                    ? new BadRequestObjectResult("The request failed")
+                    : (ActionResult)new OkObjectResult($"Data for id {id} deleted succesfully");
         }
     }
 
